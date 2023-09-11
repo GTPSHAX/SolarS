39b534f41d4aaf98af0f5806b79d4527d8c0f36f
GTPSHAX <143262216+GTPSHAX@users.noreply.github.com>
2023-09-11 20:51:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const starContainer1 = document.querySelector(".bintangs1");
     const starContainer2 = document.querySelector(".bintangs2");
-    const numberOfStars = 50; // Ganti dengan jumlah bintang yang Anda inginkan
+    const numberOfStars = 20; // Ganti dengan jumlah bintang yang Anda inginkan
     for (let i = 1; i <= numberOfStars; i++) {
         const star1 = document.createElement("div");
         star1.className = "bi bintangs1";
